3b9c53a4d0f9481200f6a840af7a0562e9aa2611
CS2071 <cs2071@kent.ac.uk>
2026-02-09 14:27:49 +0000

    Improved it

--- challenge.py
+++ challenge.py
@@ -1,4 +1,5 @@
 command_batch = ["MOVE 10", "TURN LEFT", "MOVE 5", "MOVE five", "DIG","MOVE 20","XÆA-12", "RETURN", "MOVE 15"]
+Set = ["MOVE", "DIG", "TURN"]
 Rover_State = {"x": 0, "y": 0, "samples": 0}
 Distance = int(0)
 Count = int(0)
@@ -10,7 +11,7 @@ while True:
     except:
         print("End Run")
         break
-    if Filtered[0] == "MOVE":
+    if Filtered[0] == Set[0]:
         try:
             Distance = Distance + int(Filtered[1])
             Rover_State.update({"y": Distance})
@@ -18,9 +19,13 @@ while True:
         except:
             print("Error: Invalid distance ignored")
             pass
-    if Filtered[0] == "TURN":
+    if Filtered[0] == Set[2]:
         print("Turning...")
-    if Filtered[0] == "DIG":
+    if Filtered[0] == Set[1]:
         print("Digging...")
         Rover_State.update({"samples": Dug+1})
+    if not Filtered[0] in Set:
+        print("Error: Unknown command sequence")
+        print(Filtered)
+
     Count = Count +1
