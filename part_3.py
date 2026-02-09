6a8bf36ec88f403d8db8da251f4311101fd4afbe
CS2071 <cs2071@kent.ac.uk>
2026-02-09 12:35:58 +0000

    not done yet

--- part_3.py
+++ part_3.py
@@ -1,1 +1,26 @@
+a = 0
+b = 0
+c = False
 
+def main():
+    Speed()
+    Coordinates()
+
+def Speed():
+    try:
+        a = int(input("Enter Motor Speed: ___"))
+    except ValueError:
+        print("Error: Corrupted Signal. Maintaining current speed.")
+
+def Coordinates():
+    while True:
+        try:
+            b = int(input("Input X Coordinate: ___"))
+            c = str(b).isnumeric()
+        except ValueError:
+            print("Incorrect Value Type")
+        if c is True:
+            break
+
+main()
+print()
