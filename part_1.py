854dd28e1fa3a9b25150eb7dd75cc6b2a9dcb647
CS2071 <cs2071@kent.ac.uk>
2026-02-09 11:33:06 +0000

    done

--- part_1.py
+++ part_1.py
@@ -1,1 +1,38 @@
+# Sample Bay
+Sample_Bay = ["Basalt", "Silica", "Iron", "Dust"]
 
+# Part 1.1
+print("-------- Part 1.1 --------")
+Upper = len(Sample_Bay) - 1
+print("First", "Last", "Total")
+print(Sample_Bay[0], Sample_Bay[Upper], Upper)
+print("")
+
+# Part 1.2
+print("-------- Part 1.2 --------")
+i = 0
+for Sample_bay in Sample_Bay:
+    print("Transmitting data for:", Sample_Bay[0+i])
+    i = i + 1
+print("")
+
+# Part 1.3
+print("-------- Part 1.3 --------")
+New_Findings = []
+a = 3
+while a > 0:
+    New_Findings.append(input("Input New Findings: ___ "))
+    a = a - 1
+else:
+    print(New_Findings)
+print("")
+
+# Part 1.4
+print("-------- Part 1.4 --------")
+if "Dust" in Sample_Bay:
+    print("Found Dust")
+    Sample_Bay.remove("Dust")
+    print("Removed Dust From Sample_Bay")
+    print(Sample_Bay)
+else:
+    print("No Dust Found In Sample_Bay")
