045db235fe64c70f957e307ef8a7f0978b89a886
CS2071 <cs2071@kent.ac.uk>
2026-02-09 13:02:50 +0000

    done

--- part_4.py
+++ part_4.py
@@ -1,1 +1,16 @@
+Travel_Log = []
 
+while True:
+    try:
+        a = int(input("Sensor Reading (Slope Angle):"))
+        if a > 45:
+            print("CRITICAL TILT! HALTING.")
+            print("Mission Terminated.")
+            print("Total steps taken:", len(Travel_Log)+1)
+            print(Travel_Log)
+            break
+        else:
+            Travel_Log.append(a)
+            print(Travel_Log)
+    except ValueError:
+        print("Sensor Glitch")
